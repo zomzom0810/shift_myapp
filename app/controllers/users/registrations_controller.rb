3a89ff7eccf055e456b5b6abd6d56e1ab2abd892
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-12-24 22:39:28 +0900

    deviseモデルコントローラー記載

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  protected
+  def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :position_id, :name, :name_kana])
+  end
+
+  def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :name])
+  end
+end

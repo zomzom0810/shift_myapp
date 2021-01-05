5150f74705b22f264fc48d38ec1c22df7b6087ba
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2021-01-05 17:53:29 +0900

    編集機能

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.includes(:user)
   end
   
   def new
@@ -9,9 +9,19 @@ class EventsController < ApplicationController
 
   def create
     event = Event.create!(event_params)
-    @events = Event.all
+    @events = Event.includes(:user)
   end
  
+  def edit
+    @event = Event.find(params[:id])
+    
+  end
+
+  def update
+    event = Event.update(event_params)
+    @events = Event.includes(:user)
+  end
+
   private
   def event_params
     params.require(:event).permit(:title, :start_time, :end_time).merge(user_id: current_user.id)

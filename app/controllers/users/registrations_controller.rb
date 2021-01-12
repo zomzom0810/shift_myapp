6c9c2b75d007866a07b66c020a96c209aada05f4
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2021-01-12 14:42:18 +0900

    ユーザー編集画面変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
   def configure_sign_up_params
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :position_id, :name, :name_kana])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :position_id, :name, :name_kana, :hourly_wage])
   end
 
   def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :position_id, :name, :name_kana, :hourly_wage])
   end
 end

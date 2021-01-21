830e7c860cefa1ce2a8ef5143b48fbcf45680c23
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2021-01-22 03:23:36 +0900

    表示を昇順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @events_by_month = Event.includes(:user).group_by { |event| event.start_time.strftime("%B") } 
+    @events_by_month = Event.includes(:user).order(start_time: "ASC").group_by { |event| event.start_time.strftime("%Y年%-m月") } 
   end
 end

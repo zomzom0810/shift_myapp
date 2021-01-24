f1187023a7a02eb67cdac0e6d7dc983a6655017a
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2021-01-24 19:07:36 +0900

    フラッシュメッセージ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          authentication_keys: [:user_name]
          
-  def update_without_current_password(params, *options)　#パスワードをなしでeditページ更新
+  def update_without_current_password(params, *options) #パスワードをなしでeditページ更新
     params.delete(:current_password)
 
     if params[:password].blank? && params[:password_confirmation].blank?
@@ -22,7 +22,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :user_name, uniqueness: true, format: { with: /\A[a-z0-9]+\z/ }  # 半角アルファベット（小文字・数値）
-    validates :position_id, numericality: { other_than: 1 }
+    validates :position_id, numericality: { other_than: 1 } #1のときは登録できない
     validates :name, uniqueness: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } # 日本語のみ
     validates :name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ } # カタカナのみ
   end
